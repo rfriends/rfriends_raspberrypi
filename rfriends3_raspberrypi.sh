3f043d981379a434e9c5597a1fba574192d3c954
mapi <rfriends2017@ymail.ne.jp>
2024-12-22 08:06:08 +0900

    Add files via upload

---
+++ rfriends3_raspberrypi.sh
@@ -0,0 +1,241 @@
+#!/bin/sh
+#
+# raspios setup
+#
+# 2021/02/15
+# 1.60 2021/10/26 add pulseaudio
+# 1.70 2021/11/01 
+# 1.80 2022/05/09 
+# 1.90 2023/07/28 rfriends3
+# 3.0.0 2023/10/30 renew
+# 3.0.1 2023/11/12
+# 3.1.0 2024/10/10
+# 3.2.0 2024/12/22
+ver=3.2.0
+# -----------------------------------------
+echo
+echo rfriends for RaspberryPi bullseye $ver
+echo
+# -----------------------------------------
+dir=`pwd`/conf
+user=`whoami`
+# -----------------------------------------
+# swap
+# -----------------------------------------
+sudo systemctl stop dphys-swapfile
+sudo sed -i "/^CONF_SWAPSIZE/cCONF_SWAPSIZE=256" /etc/dphys-swapfile
+sudo systemctl enable dphys-swapfile
+# -----------------------------------------
+# swappiness
+# -----------------------------------------
+sudo sed -i "/^vm.swappiness/d" /etc/sysctl.conf
+sudo sed -i '$ avm.swappiness = 1' /etc/sysctl.conf
+# -----------------------------------------
+# exim4
+# -----------------------------------------
+sudo apt -y install exim4
+# -----------------------------------------
+sudo raspi-config nonint do_boot_wait 0
+sudo raspi-config nonint do_memory_split 16
+
+# NTP時刻同期化を有効化
+sudo timedatectl set-ntp true
+# -----------------------------------------
+# 不要デーモンのoff
+#
+sudo apt -y install sysv-rc-conf
+
+sudo sysv-rc-conf dbus off
+sudo sysv-rc-conf triggerhappy off
+sudo sysv-rc-conf alsa-utils off
+sudo sysv-rc-conf lightdm off
+sudo sysv-rc-conf motd off
+sudo sysv-rc-conf plymouth off
+# -----------------------------------------
+# コンソール
+
+#sudo apt -y install ttf-kochi-gothic xfonts-intl-japanese xfonts-intl-japanese-big xfont-kaname
+#sudo apt -y  install ttf-kochi-gothic xfonts-intl-japanese xfonts-intl-japanese-big
+#sudo apt -y install uim uim-anthy
+#sudo apt -y install jfbterm
+
+#sudo apt install fbterm fonts-ipafont uim-fep uim-mozc
+#sudo apt install gpm                # for mouse
+#sudo chmod u+s /usr/bin/fbterm
+# -----------------------------------------
+# ディレクトリを作成
+#
+#sudo mkdir -p /tmp
+sudo mkdir -p /var/tmp
+sudo mkdir -p /var/log/ConsoleKit
+sudo mkdir -p /var/log/fsck
+sudo mkdir -p /var/log/apt
+sudo mkdir -p /var/log/ntpstats
+#sudo chown ntp.ntp /var/log/ntpstats
+# -----------------------------------------
+# Lastlog, wtmp ,btmp の空ファイルを作成
+#
+sudo touch /var/log/lastlog
+sudo touch /var/log/wtmp
+sudo touch /var/log/btmp
+sudo chmod 664 /var/log/lastlog
+sudo chmod 664 /var/log/wtmp
+sudo chmod 600 /var/log/btmp
+sudo chown root.utmp /var/log/lastlog
+sudo chown root.utmp /var/log/wtmp
+sudo chown root.utmp /var/log/btmp
+
+#sudo rm -rf /tmp
+#sudo rm -rf /var/tmp
+#sudo rm -rf /var/log
+
+#sudo find /var/log/ -type f -name \* -exec cp -f /dev/null {} \;
+# -----------------------------------------
+# ログ出力を減らす
+#
+sudo mv /etc/rsyslog.conf /etc/rsyslog.conf.org
+sudo cp -p $dir/rsyslog.conf /etc/rsyslog.conf
+sudo chown root:root /etc/rsyslog.conf
+
+sudo sed -i -e 's/^daemon.*/#daemon.*/' /etc/rsyslog.conf
+sudo sed -i -e 's/^lpr.*/#lpr.*/' /etc/rsyslog.conf
+sudo sed -i -e 's/^mail.*/#mail.*/' /etc/rsyslog.conf
+sudo sed -i -e 's/^user.*/#user.*/' /etc/rsyslog.conf
+
+sudo sed -i -e 's/^mail.info/#mail.info/' /etc/rsyslog.conf
+sudo sed -i -e 's/^mail.warn/#mail.warn/' /etc/rsyslog.conf
+sudo sed -i -e 's/^mail.err/#mail.err/' /etc/rsyslog.conf
+
+sudo sed -i -e 's/^news.crit/#news.crit/' /etc/rsyslog.conf
+sudo sed -i -e 's/^news.err/#news.err/' /etc/rsyslog.conf
+sudo sed -i -e 's/^news.notice/#news.notice/' /etc/rsyslog.conf
+# -----------------------------------------
+# ログローテート
+#
+sudo sed -i -e 's/rotate 4/rotate 1/' /etc/logrotate.conf
+#
+cd /etc/logrotate.d
+sudo sed -i -e 's/rotate 12/rotate 1/' alternatives
+sudo sed -i -e 's/rotate 12/rotate 1/' apt
+#sudo sed -i -e 's/rotate 1/rotate 1/' btmp
+#sudo sed -i -e 's/rotate 1/rotate 1/' wtmp
+sudo sed -i -e 's/rotate 12/rotate 1/' dpkg
+sudo sed -i -e 's/rotate 7/rotate 1/'  rsyslog
+sudo sed -i -e 's/rotate 4/rotate 1/'  rsyslog
+# -----------------------------------------
+# rc.localを設定する
+#
+sudo mv -n /etc/rc.local /etc/rc.local.org
+sudo cp -p $dir/rc.local /etc/rc.local
+sudo chmod +x /etc/rc.local
+sudo chown root:root /etc/rc.local
+# -----------------------------------------
+# localeを設定する
+#
+#sudo mv -n /etc/default/locale /etc/default/local.org
+#sudo cp -p $dir/locale /etc/default/locale
+#sudo chmod 644 /etc/default/locale
+# -----------------------------------------
+# .bashrcを設定する
+#
+cd  ~/
+sudo mv -n .bashrc .bashrc.org
+sudo cp -p $dir/.bashrc .bashrc
+sudo chmod 644 .bashrc
+# -----------------------------------------
+# .vimrcを設定する
+#
+cd  ~/
+sudo mv -n .vimrc .vimrc.org
+sudo cp -p $dir/.vimrc .vimrc
+sudo chmod 644 .vimrc
+# -----------------------------------------
+# テンポラリ領域をtmpfs（Ramdisk上）に設定する
+#
+grep rfriends /etc/fstab
+if [ $? = 1 ]; then
+cat <<EOF | sudo tee -a /etc/fstab > /dev/null
+#
+# mount ramdisk /tmp,/var/tmp,/var/log
+tmpfs /tmp     tmpfs defaults,size=64m,noatime,mode=1777 0 0
+tmpfs /var/tmp tmpfs defaults,size=16m,noatime,mode=1777 0 0
+tmpfs /var/log tmpfs defaults,size=32m,noatime,mode=0755 0 0
+#
+# mount ramdisk $user/tmp
+tmpfs $user/tmp tmpfs defaults,size=320m,noatime,mode=0777 0 0
+#
+# mount usb memory $user/smbdir
+#
+#UUID= $user/smbdir   ext4    defaults    0   0
+#PARTUUID= $user/smbdir/usbdisk exfat-fuse  nofail,defaults,nonempty,noatime,uid=1000,gid=1000 0 0
+
+EOF
+else echo "already exist"
+fi
+# -----------------------------------------
+cd  ~/
+mkdir -p $user/tmp
+mkdir -p $user/smbdir/usr2
+
+sudo apt install git
+git clone https://github.com/rfriends/rfriends_ubuntu.git
+cd rfriends_ubuntu
+sh rfriends3_ubuntu.sh
+# -----------------------------------------
+cat <<EOF | sudo tee ~/rfriends3/config/usrdir.ini > /dev/null
+#
+# 書換不可
+#
+usrdir = "$user/smbdir/usr2/"
+tmpdir = "$user/tmp/"
+EOF
+# -----------------------------------------
+# アプリのインストール
+
+sudo apt -y install samba
+#sudo apt -y install vsftpd
+sudo apt install -y lighttpd php-cgi
+# -----------------------------------------
+# samba setup
+#
+# log
+sudo mkdir -p /var/log/samba
+sudo chown root.adm /var/log/samba
+
+sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.org
+sudo cp -p $dir/smb.conf /etc/samba/smb.conf
+sudo chown root:root /etc/samba/smb.conf
+
+sudo systemctl restart smbd nmbd
+# -----------------------------------------
+# ftp setup
+#
+#sudo systemctl enable vsftpd
+# -----------------------------------------
+# setup lighttpd
+#
+sudo mv -n /etc/lighttpd/conf-available/15-fastcgi-php.conf /etc/lighttpd/conf-available/15-fastcgi-php.conf.org
+sudo cp -p $dir/15-fastcgi-php.conf /etc/lighttpd/conf-available/.
+sudo chown root:root /etc/lighttpd/conf-available/15-fastcgi-php.conf
+
+sudo mv -n /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.org
+sudo cp -p $dir/lighttpd.conf /etc/lighttpd/.
+sudo chown root:root /etc/lighttpd/lighttpd.conf
+
+sudo cp -p $dir/rfriends3_boot.txt $user/rfriends3/.
+
+mkdir -p $user/lighttpd/uploads/
+sudo lighttpd-enable-mod fastcgi
+sudo lighttpd-enable-mod fastcgi-php
+
+sudo systemctl enable lighttpd
+# -----------------------------------------
+cd /etc/logrotate.d
+sudo sed -i -e 's/rotate 7/rotate 1/g' samba
+#sudo sed -i -e 's/rotate 4/rotate 1/' vsftpd
+sudo sed -i -e 's/rotate 10/rotate 1/' exim4-base
+sudo sed -i -e 's/rotate 10/rotate 1/' exim4-paniclog
+# -----------------------------------------
+sudo touch /boot/rf3info
+exit 0
+# -----------------------------------------
