a41eec7f17fbcca8aadd0648834c7d27247e0d33
mapi <rfriends2017@ymail.ne.jp>
2024-12-23 03:00:46 +0900

    Update lighter_weight.sh

--- lighter_weight.sh
+++ lighter_weight.sh
@@ -22,26 +22,6 @@ user=`whoami`
 # システムの軽量化
 # =========================================
 # -----------------------------------------
-# ディレクトリを作成
-# -----------------------------------------
-sudo mkdir -p /var/tmp
-sudo mkdir -p /var/log/ConsoleKit
-sudo mkdir -p /var/log/fsck
-sudo mkdir -p /var/log/apt
-sudo mkdir -p /var/log/ntpstats
-# -----------------------------------------
-# Lastlog, wtmp ,btmp の空ファイルを作成
-# -----------------------------------------
-sudo touch /var/log/lastlog
-sudo touch /var/log/wtmp
-sudo touch /var/log/btmp
-sudo chmod 664 /var/log/lastlog
-sudo chmod 664 /var/log/wtmp
-sudo chmod 600 /var/log/btmp
-sudo chown root.utmp /var/log/lastlog
-sudo chown root.utmp /var/log/wtmp
-sudo chown root.utmp /var/log/btmp
-# -----------------------------------------
 # ログ出力を減らす
 # -----------------------------------------
 sudo cp -n /etc/rsyslog.conf /etc/rsyslog.conf.org
@@ -49,10 +29,10 @@ sudo cp -n /etc/rsyslog.conf /etc/rsyslog.conf.org
 sudo sed -i "/^mail.info/s/^/#/" /etc/rsyslog.conf
 sudo sed -i "/^mail.warn/s/^/#/" /etc/rsyslog.conf
 sudo sed -i "/^mail.err/s/^/#/"  /etc/rsyslog.conf
+sudo sed -i "/^mail./s/^/#/"   /etc/rsyslog.conf
 
 sudo sed -i "/^daemon./s/^/#/" /etc/rsyslog.conf
 sudo sed -i "/^lpr./s/^/#/"    /etc/rsyslog.conf
-sudo sed -i "/^mail./s/^/#/"   /etc/rsyslog.conf
 sudo sed -i "/^user./s/^/#/"   /etc/rsyslog.conf
 # -----------------------------------------
 # ログローテートを減らす
@@ -63,10 +43,10 @@ cd /etc/logrotate.d
 sudo sed -i -e 's/rotate 12/rotate 1/' alternatives
 sudo sed -i -e 's/rotate 12/rotate 1/' apt
 #sudo sed -i -e 's/rotate 1/rotate 1/' btmp
-#sudo sed -i -e 's/rotate 1/rotate 1/' wtmp
 sudo sed -i -e 's/rotate 12/rotate 1/' dpkg
-sudo sed -i -e 's/rotate 7/rotate 1/'  rsyslog
+sudo sed -i -e 's/rotate 4/rotate 1/'  ppp
 sudo sed -i -e 's/rotate 4/rotate 1/'  rsyslog
+#sudo sed -i -e 's/rotate 1/rotate 1/' wtmp
 
 sudo sed -i -e 's/rotate 7/rotate 1/g' samba
 sudo sed -i -e 's/rotate 10/rotate 1/' exim4-base
