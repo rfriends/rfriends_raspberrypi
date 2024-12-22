4250a9ef1f0bfd165ea2ff92efa899900b75f1df
mapi <rfriends2017@ymail.ne.jp>
2024-12-23 02:04:41 +0900

    Update rfriends3_raspberrypi.sh

--- rfriends3_raspberrypi.sh
+++ rfriends3_raspberrypi.sh
@@ -101,10 +101,17 @@ fi
 # =========================================
 # システムの軽量化
 # =========================================
-if [ $os = 'bookwarm' ]; then
-  sh $dir/lighter_weight12.sh
-else
-  sh $dir/lighter_weight.sh
+echo
+echo "システムの軽量化をします　(y/n) ?"
+echo
+echo "実行しますか　(y/n) ?"
+read ans
+if [ "$ans" = "y" ]; then
+    if [ $os = 'bookwarm' ]; then
+      sh $dir/lighter_weight12.sh
+    else
+      sh $dir/lighter_weight.sh
+    fi
 fi
 # =========================================
 # 終了
